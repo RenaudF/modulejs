adde3a2d419cfd7bb86772b15b00116128807132
Renaud Fontana <sirgzu@hotmail.com>
2015-06-27 13:48:44 +0100

    added warn statement for double registration

--- module.js
+++ module.js
@@ -6,6 +6,7 @@ modulejs = function(){
 	moduleLoader.register = function(name, module){
 		if (!this.moduleMap.hasOwnProperty(name))
 			Object.defineProperty(this.moduleMap, name, {value: module(), enumerable: true});
+		else console.warn('module ['+name+'] is already registered.');
 	}
 	return Object.freeze(moduleLoader);
 }
